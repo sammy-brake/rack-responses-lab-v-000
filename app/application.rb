276f541817b2a7457389b5bbc72e9357da08115c
Samantha brake <sammy.brake@gmail.com>
2019-03-01 00:14:18 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,9 @@
 class Application
 
   def call(env)
-    binding.pry
+    
     resp = Rack::Response.new
     resp.write Time.now.hour < 12 ? 'Good Morning' : 'Good Afternoon'
-    
     resp.finish
   end
 
